b61e06653c76c75277a4c7e37a04dd096be4c6a9
Martin O. Pollard <mp15@sanger.ac.uk>
2022-01-31 21:11:54 +0000

    Reformat and clean up generate

--- generatepresigned.py
+++ generatepresigned.py
@@ -4,11 +4,16 @@ import boto3
 import requests
 import json
 
-client = boto3.client('s3', endpoint_url ='https://cog.sanger.ac.uk')
+client = boto3.client('s3', endpoint_url ='https://it_randd.cog.sanger.ac.uk')
 
 Fields = {"Content-Type":"application/json"}
 
-res = client.generate_presigned_post('it_randd','file4.txt',Fields=Fields,Conditions=[{'Content-Type':'application/json'},["content-length-range", 1, 10485760], ["starts-with", "$key", "results/"]])
+res = client.generate_presigned_post('it_randd',
+                                     'results/sample_result.json',
+                                     Fields=Fields,
+                                     Conditions=[{'Content-Type':'application/json'},
+                                                 ["content-length-range", 1, 10485760],
+                                                 ["starts-with", "$key", "results/"]])
 print(res)
 with open("demofile2.txt", "a") as f:
     f.write(json.dumps(res))
