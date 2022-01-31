cdafe303e6dd9d8ba5f3095fe291d9fe4bb430b0
Martin O. Pollard <mp15@sanger.ac.uk>
2022-01-31 21:13:09 +0000

    Reformat and clean up generate

--- generatepresigned.py
+++ generatepresigned.py
@@ -4,7 +4,7 @@ import boto3
 import requests
 import json
 
-client = boto3.client('s3', endpoint_url ='https://it_randd.cog.sanger.ac.uk')
+client = boto3.client('s3', endpoint_url ='https://cog.sanger.ac.uk')
 
 Fields = {"Content-Type":"application/json"}
 
@@ -15,7 +15,7 @@ res = client.generate_presigned_post('it_randd',
                                                  ["content-length-range", 1, 10485760],
                                                  ["starts-with", "$key", "results/"]])
 print(res)
-with open("demofile2.txt", "a") as f:
+with open("post_signed_url.json", "a") as f:
     f.write(json.dumps(res))
 
 #with open('a.txt', 'rb') as f: 
