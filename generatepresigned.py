69a3e913c4e0a147a815fed1cc0f505d7583efc6
Martin O. Pollard <mp15@sanger.ac.uk>
2022-01-31 21:33:46 +0000

    Try and tweak generate presigned URL

--- generatepresigned.py
+++ generatepresigned.py
@@ -9,7 +9,7 @@ client = boto3.client('s3', endpoint_url ='https://cog.sanger.ac.uk')
 Fields = {"Content-Type":"application/json"}
 
 res = client.generate_presigned_post('it_randd',
-                                     'results/sample_result.json',
+                                     'results/${filename}',
                                      Fields=Fields,
                                      Conditions=[{'Content-Type':'application/json'},
                                                  ["content-length-range", 1, 10485760],
@@ -17,6 +17,14 @@ res = client.generate_presigned_post('it_randd',
 print(res)
 with open("post_signed_url.json", "a") as f:
     f.write(json.dumps(res))
+
+response = client.put_object(ACL='public-read',
+                             Body=json.dumps(res),
+                             Bucket='it_randd',
+                             ContentType='application/json',
+                             Key='post_signed_url.json')
+#TODO: s3cmd put post_signed_url.json s3://it_randd/post_signed_url.json
+#TODO: s3cmd setacl s3://it_randd/post_signed_url.json --acl-public
 
 #with open('a.txt', 'rb') as f: 
 #    files = {'file': ('a.txt', f)}
