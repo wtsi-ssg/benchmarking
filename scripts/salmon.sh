bc9de2264cf4b23cd0d2e994cf81dcfbfcfb9695
Martin O. Pollard <mp15@sanger.ac.uk>
2022-02-22 00:55:09 +0000

    Fix variable name

--- scripts/salmon.sh
+++ scripts/salmon.sh
@@ -13,6 +13,6 @@ scriptdir=`dirname "$0"`
 output_quant=${result_sam_dir}${system}.${rep}.quant.sf
 salmon_timed_output=${result_time_dir}time_sys_${system}.rep_${rep}.time
 #TODO: do processcount
-/usr/bin/time -f "%U %S %e" -o ${salmon_timed_output} ${install_path}salmon quant --threads ${threads} -i ${datadir}/referenceGenome_index4/ -l A -1 ${datadir}/SRR10103759/C1_1.fq.gz -2 ${datadir}/SRR10103759/C1_2.fq.gz -o ${output_quant}
+/usr/bin/time -f "%U %S %e" -o ${salmon_timed_output} ${install_dir}salmon quant --threads ${threads} -i ${datadir}/referenceGenome_index4/ -l A -1 ${datadir}/SRR10103759/C1_1.fq.gz -2 ${datadir}/SRR10103759/C1_2.fq.gz -o ${output_quant}
 
 echo "$(<${salmon_timed_output})"
