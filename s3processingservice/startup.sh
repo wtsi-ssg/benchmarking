b75f50be7af0c5ee42326268d46e726d40d56367
Martin Pollard <mp15@sanger.ac.uk>
2022-03-07 15:04:58 +0000

    Correct filename

--- s3processingservice/startup.sh
+++ s3processingservice/startup.sh
@@ -2,6 +2,10 @@ ACCESS_KEY=$1
 SECRET_ACCESS_KEY=$2
 DSN=$3
 RABBIT_PASSWORD=`openssl rand -base64 32`
-docker run -v ./rabbitmq.conf:/etc/rabbitmq/rabbitmq.conf:ro ./definitions.json:/etc/rabbitmq/definitions.json:ro -d --hostname s3-rabbit --name s3-rabbit -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=$RABBIT_PASSWORD rabbitmq:3-management
+docker create --hostname s3-rabbit --name s3-rabbit -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS="$RABBIT_PASSWORD" rabbitmq:3-management
+docker cp rabbitmq.conf s3-rabbit:/etc/rabbitmq/rabbitmq.conf
+docker cp definitions.json s3-rabbit:/etc/rabbitmq/definitions.json
+docker start s3-rabbit
+
 RABBITMQ_IP=`docker inspect s3-rabbit | jq '.[].NetworkSettings.Networks.bridge.IPAddress'`
-docker run -d --hostname s3-consume -name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBITMQ_IP $DSN
+docker run -d --hostname s3-consume --name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBITMQ_IP $DSN
