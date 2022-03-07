3ed6445427c475726b1bbdc3767c54edb9276df6
Martin Pollard <mp15@sanger.ac.uk>
2022-03-07 15:30:47 +0000

    Add rabbit username and password to script

--- s3processingservice/imports3.py
+++ s3processingservice/imports3.py
@@ -55,6 +55,10 @@ def dump_message(message):
 parser = argparse.ArgumentParser(description='Check queue for files to download from S3.')
 parser.add_argument('host', metavar='host', type=str, nargs='?',
                     help='rabbitmq host', default='localhost')
+parser.add_argument('user', metavar='user', type=str, nargs='?',
+                    help='rabbitmq username', default='localhost')
+parser.add_argument('pass', metavar='pass', type=str, nargs='?',
+                    help='rabbitmq password', default='localhost')
 parser.add_argument('dsn', metavar='host', type=str, nargs='?',
                     help='postgres host', default='dbname=benchmarking user=postgres password=postgres')
 
@@ -65,8 +69,8 @@ with open('jsonschema.json', mode='r') as f:
     schema = json.load(f)
 
 # Connect to Rabbit MQ server providing S3 notifications
-conn = Connection(host=args.host, userid="guest",
-                  password="guest", virtual_host="/")
+conn = Connection(host=args.host, userid=args.user,
+                  password=args.pass, virtual_host="/")
 conn.connect()
 
 # Set up channel to recieve from queue
--- s3processingservice/startup.sh
+++ s3processingservice/startup.sh
@@ -7,5 +7,5 @@ docker cp rabbitmq.conf s3-rabbit:/etc/rabbitmq/rabbitmq.conf
 docker cp definitions.json s3-rabbit:/etc/rabbitmq/definitions.json
 docker start s3-rabbit
 
-RABBITMQ_IP=`docker inspect s3-rabbit | jq '.[].NetworkSettings.Networks.bridge.IPAddress'`
-docker run -d --hostname s3-consume --name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBITMQ_IP $DSN
+RABBITMQ_IP=`docker inspect s3-rabbit | jq --raw-output '.[].NetworkSettings.Networks.bridge.IPAddress'`
+docker run -d --hostname s3-consume --name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBITMQ_IP $RABBIT_USER $RABBIT_PASSWORD "$DSN"
