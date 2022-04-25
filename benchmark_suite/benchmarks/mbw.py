900b23a9d98e35c3b8b5595544131402256094b3
Martin O. Pollard <mp15@sanger.ac.uk>
2022-04-25 16:02:16 +0100

    mbw Fix arguments and output parse

--- benchmark_suite/benchmarks/mbw.py
+++ benchmark_suite/benchmarks/mbw.py
@@ -15,7 +15,7 @@ class Plugin(benchmarkessentials.BenchmarkPlugin):
         return {"mbw": MBW}
 
 class MBW(benchmarkessentials.Benchmark):
-    def __init__(self, install_dir, arguments="-a", program=None, programversion=None, **kwargs):
+    def __init__(self, install_dir, arguments="", program=None, programversion=None, **kwargs):
         super().__init__(**kwargs)
         self.arguments = arguments
         self.install_path = "{}{}/".format(install_dir, program+"-v"+programversion)
@@ -31,16 +31,16 @@ class MBW(benchmarkessentials.Benchmark):
             if str.isalpha(splits[0]) == True:
                 next
             record = { 'iteration' : splits[0],
-                'method' : splits[2],
-                'elapsed' : splits[4],
-                'MiB' : splits[6],
-                'copy' : splits[8]
+                'method' : splits[1].split(':')[1],
+                'elapsed' : splits[2].split(':')[1],
+                'MiB' : splits[3].split(':')[1],
+                'copy' : splits[4].split(':')[1]
             }
             results.append(record)
         return results
 
     def run(self):
-        with subprocess.Popen([self.install_path + "/mbw", "-q"+self.arguments], stdout=subprocess.PIPE, universal_newlines=True) as process:
+        with subprocess.Popen([self.install_path + "/mbw", "-q", self.arguments], stdout=subprocess.PIPE, universal_newlines=True) as process:
             stdout, _ = process.communicate()
             results = self._parse_mbw_output(stdout)
 
