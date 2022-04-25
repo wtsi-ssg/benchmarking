a0ee9615d4276b7e4cb076c7ef5c3284f4b0d6c8
Martin O. Pollard <mp15@sanger.ac.uk>
2022-04-25 15:03:24 +0100

    MBW benchmark

---
+++ benchmark_suite/benchmarks/mbw.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import pathlib
+import subprocess
+import time
+
+from benchmark_suite import benchmarkessentials
+import pandas as pd
+
+
+class Plugin(benchmarkessentials.BenchmarkPlugin):
+    def get_benchmarks(self):
+        return {"mbw": MBW}
+
+class MBW(benchmarkessentials.Benchmark):
+    def __init__(self, install_dir, arguments="-a", program=None, programversion=None, **kwargs):
+        super().__init__(**kwargs)
+        self.arguments = arguments
+        self.install_path = "{}{}/src/current".format(install_dir, program+"-v"+programversion)
+
+    def get_name(self):
+        return "mbw"
+
+    def _parse_mbw_output(self, stdout_text: str):
+        lines = stdout_text.splitlines()
+        results = ()
+        for line in lines:
+            splits = line.split('\t')
+            if str.isalpha(splits[0]) == True:
+                next
+            record = { 'iteration' : splits[0],
+                'method' : splits[2],
+                'elapsed' : splits[4],
+                'MiB' : splits[6],
+                'copy' : splits[8]
+            }
+            results.append(record)
+        return results
+
+    def run(self):
+        iozone_result_file = self._get_iozone_xls()
+        with subprocess.Popen([self.install_path + "/mbw", "-q"+self.arguments], stdout=subprocess.PIPE, universal_newlines=True) as process:
+            stdout, _ = process.communicate()
+            results = self._parse_mbw_output(stdout)
+
+        return {"results": self._parse_iozone_xls(iozone_result_file)} if stdout else {"results": ""}
+
