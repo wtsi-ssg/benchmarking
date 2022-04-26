6e40b47e51932d6a68b52036bc1a118df11df285
Martin O. Pollard <mp15@sanger.ac.uk>
2022-04-26 14:00:46 +0100

    Update kwargs in multithread

--- benchmark_suite/benchmarks/multithreaded.py
+++ benchmark_suite/benchmarks/multithreaded.py
@@ -18,8 +18,8 @@ class Plugin(benchmarkessentials.BenchmarkPlugin):
         return {"multithreaded": MultiThread}
 
 class MultiThread(benchmarkessentials.Benchmark):
-    def __init__(self, *args, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, command, install_dir, tag=None, shell=False, datadir=None, dataset_file=None, result_dir=None, clear_caches=False, repeats=1, program=None, programversion=None, dataset_tag=None, step=None, threads=None, process_thread=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.program = program
         self.programversion = programversion
         self.install_dir = install_dir
