995de9930e590926a9c723e5bc821576a679a7a1
Martin O. Pollard <mp15@sanger.ac.uk>
2022-04-26 13:51:30 +0100

    Don't overwrite CFLAGS for bwa-mem2

--- prepareScript.py
+++ prepareScript.py
@@ -138,7 +138,7 @@ def download_and_install_programs(settings_list, install_dir):
                     if program_name in ["bwa"]:
                         subprocess.check_call(["make CFLAGS='-g -Wall -Wno-unused-function -O3 -march=native'"], shell=True, cwd=install_dir+name_and_version)
                     if program_name in ["bwa-mem2"]:
-                        subprocess.check_call(["make arch=native CFLAGS='-g -Wall -Wno-unused-function -O3'"], shell=True, cwd=install_dir+name_and_version)
+                        subprocess.check_call(["make arch=native"], shell=True, cwd=install_dir+name_and_version)
 
         if not path.exists(path_to_program):
             print("Entry for this tool is not found in the binaryAddress list. Please update the list and run again!")
