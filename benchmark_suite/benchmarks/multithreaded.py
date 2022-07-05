01ece54d5fa37cb2289458c3004a808ba320b4ba
Martin O. Pollard <mp15@sanger.ac.uk>
2022-07-05 17:36:59 +0100

    Update validation schema and set data to numeric type

--- benchmark_suite/benchmarks/multithreaded.py
+++ benchmark_suite/benchmarks/multithreaded.py
@@ -110,7 +110,7 @@ class MultiThread(benchmarkessentials.Benchmark):
                 perfprocess.communicate()
 
                 with os.fdopen(perftempfd, "w+") as perftempfo:
-                    f = lambda x: {"value": x[0], "units": x[1], "measure": x[2].strip('\"')}
+                    f = lambda x: {"value": float(x[0]), "units": x[1], "measure": x[2].strip('\"')}
                     power_string_list = [line.strip().split(",")for line in perftempfo.readlines()]
                     power_list = [f(line) for line in power_string_list if len(line) == 7 ]
 
