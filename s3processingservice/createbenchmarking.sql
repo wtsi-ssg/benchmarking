0bba4fea7dbec882628d705250ec6e8f0908ea5c
Martin O. Pollard <mp15@sanger.ac.uk>
2022-08-18 13:16:51 +0100

    Improve cpu_results view and add mbw_results view

--- s3processingservice/createbenchmarking.sql
+++ s3processingservice/createbenchmarking.sql
@@ -16,41 +16,66 @@ CREATE TABLE public.returned_results
 ALTER TABLE IF EXISTS public.returned_results
     OWNER to benchmarking;
 
+-- View: public.cpu_results
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS public.cpu_results
 TABLESPACE pg_default
 AS
  SELECT benchmark_data.nickname,
     benchmark_data.benchmark_type,
-    benchmark_data.processes,
-    benchmark_data.threads,
-    benchmark_data.usercpu,
-    benchmark_data.elapsed,
-    benchmark_data.maxrss,
-    (benchmark_data.powerl -> 'cpu_energy'::text) -> 'value'::text AS cpu_energy,
-    (benchmark_data.powerl -> 'ram_energy'::text) -> 'value'::text AS ram_energy
+    benchmark_data.processes::integer,
+    benchmark_data.threads::integer,
+    benchmark_data.usercpu::numeric,
+    benchmark_data.elapsed::numeric,
+    benchmark_data.maxrss::integer,
+    ((benchmark_data.powerl -> 'cpu_energy'::text) ->> 'value'::text)::numeric AS cpu_energy,
+    ((benchmark_data.powerl -> 'ram_energy'::text) ->> 'value'::text)::numeric AS ram_energy
    FROM ( SELECT benchmark_runs.nickname,
             benchmark_runs.benchmark_type,
             benchmark_runs.processes,
             benchmark_runs.threads,
-            benchmark_runs.runs -> 'user'::text AS usercpu,
-            benchmark_runs.runs -> 'elapsed'::text AS elapsed,
-            benchmark_runs.runs -> 'maxrss'::text AS maxrss,
+            benchmark_runs.runs ->> 'user'::text AS usercpu,
+            benchmark_runs.runs ->> 'elapsed'::text AS elapsed,
+            benchmark_runs.runs ->> 'maxrss'::text AS maxrss,
             benchmark_runs.runs -> 'power'::text AS powerl
            FROM ( SELECT benchmarks.nickname,
                     benchmarks.benchmark_type,
-                    benchmarks.benchmark_types -> 'processes'::text AS processes,
-                    benchmarks.benchmark_types -> 'threads'::text AS threads,
+                    benchmarks.benchmark_types ->> 'processes'::text AS processes,
+                    benchmarks.benchmark_types ->> 'threads'::text AS threads,
                     jsonb_array_elements(benchmarks.benchmark_types -> 'runs'::text) AS runs
                    FROM ( SELECT hosts.nickname,
                             hosts.benchmark_type,
                             jsonb_array_elements((jsonb_array_elements(hosts.benchmark_types -> hosts.benchmark_type) -> 'results'::text) -> 'configurations'::text) AS benchmark_types
-                           FROM ( SELECT maindata.jsondata -> 'nickname'::text AS nickname,
-                                    jsonb_object_keys(((maindata.jsondata -> 'results'::text) -> 'CPU'::text) -> 'benchmarks'::text) AS benchmark_type,
-                                    ((maindata.jsondata -> 'results'::text) -> 'CPU'::text) -> 'benchmarks'::text AS benchmark_types
-                                   FROM ( SELECT returned_results.jsondata
-   FROM returned_results) maindata) hosts
+                           FROM ( SELECT returned_results.jsondata ->> 'nickname'::text AS nickname,
+                                    jsonb_object_keys(((returned_results.jsondata -> 'results'::text) -> 'CPU'::text) -> 'benchmarks'::text) AS benchmark_type,
+                                    ((returned_results.jsondata -> 'results'::text) -> 'CPU'::text) -> 'benchmarks'::text AS benchmark_types
+                                   FROM returned_results) hosts
                           WHERE hosts.benchmark_type ~~ 'multithreaded_%'::text) benchmarks) benchmark_runs) benchmark_data
 WITH DATA;
 
 ALTER TABLE IF EXISTS public.cpu_results
+    OWNER TO benchmarking;
+
+-- View: public.mbw_results
+
+-- DROP MATERIALIZED VIEW IF EXISTS public.mbw_results;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.mbw_results
+TABLESPACE pg_default
+AS
+ SELECT benchmarks.nickname,
+    (benchmarks.mbw_results ->> 'MiB'::text)::numeric AS mib,
+    split_part(benchmarks.mbw_results ->> 'copy'::text, ' ', 1)::numeric AS copyrate,
+    split_part(benchmarks.mbw_results ->> 'copy'::text, ' ', 2) AS copyrateunits,
+    (benchmarks.mbw_results ->> 'elapsed'::text)::numeric AS elapsed,
+    (benchmarks.mbw_results ->> 'iteration'::text)::integer AS iteration,
+    benchmarks.mbw_results ->> 'method'::text AS method
+   FROM ( SELECT hosts.nickname,
+            jsonb_array_elements(jsonb_array_elements(hosts.benchmark_types -> 'mbw'::text) -> 'results'::text) AS mbw_results
+           FROM ( SELECT returned_results.jsondata ->> 'nickname'::text AS nickname,
+                    ((returned_results.jsondata -> 'results'::text) -> 'CPU'::text) -> 'benchmarks'::text AS benchmark_types
+                   FROM returned_results) hosts) benchmarks
+WITH DATA;
+
+ALTER TABLE IF EXISTS public.mbw_results
     OWNER TO benchmarking;
