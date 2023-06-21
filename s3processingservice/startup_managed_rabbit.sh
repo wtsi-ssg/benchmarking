54e8367f75e799dc0a4f61ba9aa94c57a64fc149
Martin O. Pollard <mp15@sanger.ac.uk>
2023-06-21 23:16:18 +0100

    Fix another bash script bug

--- s3processingservice/startup_managed_rabbit.sh
+++ s3processingservice/startup_managed_rabbit.sh
@@ -7,4 +7,4 @@ RABBIT_HOST=$4
 RABBIT_VHOST=$5
 RABBIT_USER=$6
 RABBIT_PASSWORD=$7
-docker run -d --hostname s3-consume --name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBITMQ_HOST $RABBIT_VHOST $RABBIT_USER $RABBIT_PASSWORD "$DSN"
+docker run -d --hostname s3-consume --name s3-consume -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY wsisci/benchmarking_s3processing $RABBIT_HOST $RABBIT_VHOST $RABBIT_USER $RABBIT_PASSWORD "$DSN"
